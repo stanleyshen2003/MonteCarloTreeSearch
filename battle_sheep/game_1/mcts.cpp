b4e1e9df555c6ab36883a29823a919eec55ecbc1
stanleyshen2003 <stanley.shen2003@gmail.com>
2024-04-09 02:45:33 +0800

    add comment

--- battle_sheep/game_1/mcts.cpp
+++ battle_sheep/game_1/mcts.cpp
@@ -14,26 +14,45 @@ struct Action {
     int x, y, n, dir;
     Action(int x, int y, int n, int dir) : x(x), y(y), n(n), dir(dir) {}
     Action() : x(-1), y(-1), n(-1), dir(-1) {}
-    string get_key() {
-        return to_string(x) + to_string(y) + to_string(n) + to_string(dir);
-    }
+    string get_key();
 };
+
+string Action::get_key() {
+    return to_string(x) + to_string(y) + to_string(n) + to_string(dir);
+}
 
 class GameState {
     public:
+        // the state of the game
         char user_state[12][12];
         int sheep_state[12][12];
         char turn;
+
+        // (TODO: move dir8 & dir4 to somewhere else to make it more memory efficient)
         int directions8[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
         int directions4[4][2] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+
+        // constructors and destructor
         GameState(int user_state[12][12], int sheep_state[12][12], char turn);
         GameState(const GameState* state);
         ~GameState();
+
+        // get all possible actions in the current state
         vector<Action> get_actions();
+
+        // get the key of the state for the map (you can ignore this)
         string get_key();
+
+        // update the state of the game according to the action
         void next_state(Action& action);
+
+        // check if the game terminates
         bool is_terminal();
+
+        // check if the player with the given turn is the winner
         bool is_winner(char turn);
+
+        // calculate the score of the player with the given turn, used in is_winner()
         int recursive_calculate(int x, int y, char person);
 };
 
@@ -181,17 +200,22 @@ int GameState::recursive_calculate(int x, int y, char person){
 
 class MCTS_agent {
     public:
+        // this map stores the number of wins and the number of loses for each state
         unordered_map<string, puu> move_map;
+
+        // config of the agent
         int max_iter, max_seconds;
+
+        // the player's turn
         char player_turn;
+
+        // constructors and destructor
         MCTS_agent(int max_iter = 2000, int max_seconds = 2, char player_turn = '1');
         ~MCTS_agent();
 
+        // decide the next step given present state (maybe adding a rollout function would be better)
         Action decide_step(GameState& state);
-
-
 };
-
 
 MCTS_agent::MCTS_agent(int max_iter, int max_seconds, char player_turn) : max_iter(max_iter), max_seconds(max_seconds), player_turn(player_turn) {
     move_map = unordered_map<string, puu>();
